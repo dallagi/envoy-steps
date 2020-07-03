fd85327034c6f3178f242289d0e3084ae21f7173
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-07-03 17:09:53 +0200

    Enrich response of flask app

--- usersvc/service.py
+++ usersvc/service.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
-from flask import Flask
+import socket
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+def enrich_response(response):
+    hostname = socket.gethostname()
+    resolved = socket.gethostbyname(socket.gethostname())
+
+    return jsonify({ 'hostname': hostname, 'resolved': resolved, **response})
 
 
 @app.route('/user/health')
 def hello():
-    return "Hello World!"
+    my_host_name = socket.gethostname()
+    my_resolved_name = socket.gethostbyname(socket.gethostname())
+
+    return enrich_response({'msg': 'Hello World!'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
